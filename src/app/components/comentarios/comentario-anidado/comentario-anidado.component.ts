ae5cc62ab078fb78a54a03f1852a7fe606ba0159
CarlosQuispeGuzman45 <113564458+CarlosQuispeGuzman45@users.noreply.github.com>
2023-07-23 14:02:55 -0500

    Agregar el contenido del id padre del comentario

--- src/app/components/comentarios/comentario-anidado/comentario-anidado.component.ts
+++ src/app/components/comentarios/comentario-anidado/comentario-anidado.component.ts
@@ -1,6 +1,6 @@
+import { Comentario } from './../../../models/comentario';
 import { ComentariosService } from 'src/app/services/comentarios.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { Comentario } from '../../../models/comentario';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogoComponent } from '../lista-comentarios/dialogo/dialogo.component';
 
@@ -12,12 +12,17 @@ import { DialogoComponent } from '../lista-comentarios/dialogo/dialogo.component
 export class ComentarioAnidadoComponent implements OnInit {
   @Input() comentario!: Comentario;
   private idMayor: number = 0;
-  constructor(private comentarioService: ComentariosService,private dialog: MatDialog) { }
+  comentarioPadre: Comentario = new Comentario();
+  constructor(private comentarioService: ComentariosService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.comentarioService.getConfirmaEliminacion().subscribe(data => {
       data == true ? this.eliminarComentario(this.idMayor) : false;
     });
+
+    this.comentarioService.listarId(this.comentario.id_padre).subscribe(data => {
+      this.comentarioPadre = data;
+    })
   }
 
   confirmar(id: number) {
